7fa7c935914777be02417662c0bbb763de14ce21
Karitham <kar@karitham.dev>
2024-01-19 14:13:52 +0100

    fix desktop icon, move miru to its own file

--- flake.nix
+++ flake.nix
@@ -7,30 +7,12 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      stdenv = pkgs.stdenv;
     in
     rec
     {
-      version = "4.5.9";
-
-      miru = pkgs.appimageTools.wrapType2 {
-        name = "miru";
-        src = pkgs.fetchurl {
-          url = "https://github.com/ThaUnknown/miru/releases/download/v${version}/linux-Miru-${version}.AppImage";
-          hash = "sha256-Ntziq6MFkw4nif9jKifmkXjhHVcwgy+9ckqDXkoYG5M=";
-        };
-        extraPkgs = pkgs: with pkgs; [ ];
-
-        meta = with stdenv.lib; {
-          name = "miru";
-          description = "Bittorrent streaming software for cats.";
-          homepage = "https://github.com/ThaUnknown/miru";
-          platform = [ system ];
-          targetPkgs = [ miru ];
-          mainProgram = "miru";
-        };
+      packages."${system}".miru = import ./miru.nix {
+        stdenv = pkgs.stdenv;
+        inherit pkgs;
       };
-
-      packages."${system}".miru = miru;
     };
 }
---
+++ miru.nix
@@ -0,0 +1,31 @@
+{ stdenv, pkgs }:
+pkgs.appimageTools.wrapType2 rec {
+  name = "miru";
+  pname = "miru";
+  version = "4.5.9";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/ThaUnknown/miru/releases/download/v${version}/linux-Miru-${version}.AppImage";
+    hash = "sha256-Ntziq6MFkw4nif9jKifmkXjhHVcwgy+9ckqDXkoYG5M=";
+  };
+
+  extraInstallCommands =
+    let
+      contents = pkgs.appimageTools.extract { inherit pname version src; };
+    in
+    ''
+      install -m 444 -D ${contents}/miru.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/miru.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
+      cp -r ${contents}/usr/share/icons $out/share
+    '';
+
+  meta = with pkgs.lib; {
+    name = "miru";
+    description = "Bittorrent streaming software for cats.";
+    homepage = "https://github.com/ThaUnknown/miru";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ karitham ];
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "miru";
+  };
+}
