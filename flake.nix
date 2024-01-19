53f9e4659348bc41b1ddda379097e5916e9f5688
Karitham <kar@karitham.dev>
2024-01-19 12:52:23 +0100

    use unstable instead

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   description = "miru";
   outputs = { self, nixpkgs }:
