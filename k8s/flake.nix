8876e3e19f3c2d5391ba274045dd623584569c6b
Kar <kar@karitham.dev>
2024-09-04 14:58:27 +0200

    Update flake.nix

--- k8s/flake.nix
+++ k8s/flake.nix
@@ -1,26 +1,22 @@
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-  }:
-    utils.lib.eachDefaultSystem (
+  outputs = {nixpkgs, ...}: {
+    devShell = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"] (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell {
+        pkgs = import nixpkgs {inherit system;};
+      in
+        pkgs.mkShell {
           buildInputs = with pkgs; [
             kubernetes-helm
-            kubernetes-helmPlugins.helm-unittest
             kubectx
             kubectl
+            kind
+            fluxcd
             sops
           ];
-        };
-      }
+        }
     );
+  };
 }
