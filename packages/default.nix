eee0a5c9a55542106e8124d8bd8b1003b9153dac
karitham <kar@karitham.dev>
2025-10-20 10:42:44 +0200

    miru->hayase

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  hayase = pkgs.callPackage ./hayase.nix {};
+in {
   df-pv = pkgs.callPackage ./df-pv.nix {};
-  miru = pkgs.callPackage ./miru.nix {};
+  miru = lib.warn "The 'miru' package is deprecated. Please use 'hayase' instead." hayase;
+  hayase = hayase;
   helm-readme-generator = pkgs.callPackage ./helm-readme-generator.nix {};
   tidb-tools = pkgs.callPackage ./tidb-tools.nix {};
   okta-cli = pkgs.callPackage ./okta-cli.nix {};
---
+++ packages/hayase.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  dpkg,
+  makeWrapper,
+  autoPatchelfHook,
+  alsa-lib,
+  at-spi2-atk,
+  at-spi2-core,
+  cairo,
+  dbus,
+  gtk3,
+  libdrm,
+  mesa,
+  nspr,
+  nss,
+  cups,
+}:
+stdenv.mkDerivation rec {
+  pname = "hayase";
+  version = "6.4.34";
+
+  src = fetchurl {
+    url = "https://github.com/hayase-app/ui/releases/download/v${version}/linux-hayase-${version}-linux.deb";
+    sha256 = "1zgxc978vd3pkfc4dqzppb6ff15gap9mcxq4zp2gd1wqm4wvyij8";
+  };
+
+  nativeBuildInputs = [dpkg makeWrapper autoPatchelfHook];
+
+  buildInputs = [
+    alsa-lib
+    at-spi2-atk
+    at-spi2-core
+    cairo
+    dbus
+    gtk3
+    libdrm
+    mesa
+    nspr
+    nss
+    cups
+  ];
+
+  unpackPhase = "dpkg-deb -x $src .";
+
+  installPhase = ''
+    mkdir -p $out/bin $out/share
+    cp -r opt/Hayase $out/share/hayase
+    cp -r usr/share/* $out/share/
+
+    makeWrapper $out/share/hayase/hayase $out/bin/hayase \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
+      --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland" \
+      --set ELECTRON_NO_UPDATER 1
+  '';
+
+  meta = with lib; {
+    description = "Anime torrent streaming app";
+    homepage = "https://github.com/hayase-app/ui";
+    license = licenses.bsl11;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [karitham];
+  };
+}
